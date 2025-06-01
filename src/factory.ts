dbe18288967fd9dd8d73527a96bf46c02243e232
DiepPk <kdt310722@gmail.com>
2025-06-01 12:54:09 +0700

    feat: add config and configs options to defineConfig function

--- src/factory.ts
+++ src/factory.ts
@@ -1,3 +1,4 @@
+import type { Linter } from 'eslint'
 import { composer } from 'eslint-flat-config-utils'
 import { antfu, comments, format, type FormatOptions, gitignore, ignores, importX, javascript, jsonc, kdt, node, perfectionist, promise, regexp, sonar, stylistic, typescript, type TypescriptOptions, unicorn, unusedImports, vue } from './configs'
 
@@ -5,9 +6,11 @@ export interface DefineConfigOptions {
     typescript?: TypescriptOptions
     format?: FormatOptions
     vue?: boolean
+    config?: Linter.Config
+    configs?: Linter.Config[]
 }
 
-export function defineConfig({ typescript: typescriptOptions = {}, format: formatOptions, vue: isVueEnabled }: DefineConfigOptions = {}) {
+export function defineConfig({ typescript: typescriptOptions = {}, format: formatOptions, vue: isVueEnabled, config, configs: configs_ }: DefineConfigOptions = {}) {
     typescriptOptions.componentExts ??= []
 
     if (isVueEnabled) {
@@ -17,7 +20,15 @@ export function defineConfig({ typescript: typescriptOptions = {}, format: forma
     const configs = composer(ignores(), gitignore(), javascript(), typescript(typescriptOptions), stylistic(), comments(), importX(), jsonc(), node(), promise(), regexp(), unusedImports(), antfu(), kdt(), format(formatOptions), perfectionist(), sonar(), unicorn())
 
     if (isVueEnabled) {
-        return configs.append(vue())
+        configs.append(vue())
+    }
+
+    if (config) {
+        configs.append(config)
+    }
+
+    if (configs_) {
+        configs.append(configs_)
     }
 
     return configs
